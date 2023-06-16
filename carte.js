ca2fa7d3a5ced5c8c671f7a9878131b349cc19e5
Cylsl <112876100+Cylsl@users.noreply.github.com>
2023-06-17 00:13:04 +0200

    Add files via upload

---
+++ carte.js
@@ -0,0 +1,7 @@
+const customCursor = document.querySelector(".custom-cursor");
+
+window.addEventListener("mousemove", handleCustomCursor);
+
+function handleCustomCursor(e) {
+  customCursor.style.transform = `translate(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%))`;
+}
